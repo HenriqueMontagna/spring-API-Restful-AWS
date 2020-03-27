33113613e8f20000efa46d403d1a8e7aa14da8e0
Henrique La Montagna <henriquelamontagna@gmail.com>
2020-03-27 12:08:56 -0300

    Initial commit

---
+++ src/main/java/com/montagna/apirestful/aws/SpringApiRestfulAwsApplication.java
@@ -0,0 +1,13 @@
+package com.montagna.apirestful.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiRestfulAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiRestfulAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/montagna/apirestful/aws/SpringApiRestfulAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.montagna.apirestful.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiRestfulAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
