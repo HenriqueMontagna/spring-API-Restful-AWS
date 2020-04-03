ad29e7aa8647c8f1d1d59f88279ddc191d884303
Henrique La Montagna <henriquelamontagna@gmail.com>
2020-04-03 18:11:36 -0300

    Create Request Repository

---
+++ src/main/java/com/montagna/apirestful/aws/repository/RequestRepository.java
@@ -0,0 +1,19 @@
+package com.montagna.apirestful.aws.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.montagna.apirestful.aws.domain.Request;
+import com.montagna.apirestful.aws.domain.enums.RequestState;
+
+@Repository
+public interface RequestRepository extends JpaRepository<Request, Long>{
+
+	public List<Request> findAllByOwnerId(Long id);
+	
+	@Query("UPDATE request SET state = ?2 WHERE id = ?1")
+	public Request updateStatus(Long id, RequestState state);
+}
