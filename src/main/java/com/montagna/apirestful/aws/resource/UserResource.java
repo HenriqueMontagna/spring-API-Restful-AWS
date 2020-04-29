70eafe2e19b9525196078070777058357706a62b
Henrique La Montagna <henriquelamontagna@gmail.com>
2020-04-29 15:27:44 -0300

    Create UserResource.java

---
+++ src/main/java/com/montagna/apirestful/aws/resource/UserResource.java
@@ -0,0 +1,68 @@
+package com.montagna.apirestful.aws.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.montagna.apirestful.aws.DTO.UserLoginDTO;
+import com.montagna.apirestful.aws.domain.Request;
+import com.montagna.apirestful.aws.domain.User;
+import com.montagna.apirestful.aws.service.RequestService;
+import com.montagna.apirestful.aws.service.UserService;
+
+@RestController
+@RequestMapping(value = "users")
+public class UserResource {
+
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private RequestService requestService;
+	
+	@PostMapping
+	public ResponseEntity<User> save(@RequestBody User user){
+		User createdUser = userService.save(user);
+		return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable("id") Long id, @RequestBody User user){
+		user.setId(id);
+		User updatedUser = userService.update(user);
+		return ResponseEntity.ok(updatedUser);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> getById(@PathVariable("id") Long id){
+		User user = userService.getById(id);
+		return ResponseEntity.ok(user);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<User>> listAll(){
+		List<User> users = userService.listAll();
+		return ResponseEntity.ok(users);
+	}
+	
+	@GetMapping("/{userId}/requests")
+	public ResponseEntity<List<Request>> listAllRequestsById(@PathVariable("userId") Long id){
+		List<Request> requests = requestService.listByOwnerId(id);
+		return ResponseEntity.ok(requests);
+	}
+	
+	@PostMapping(value = "/login")
+	public ResponseEntity<User> login(@RequestBody UserLoginDTO user){
+		User loggedUser = userService.login(user.getEmail(), user.getPassword());
+		return ResponseEntity.ok(loggedUser);
+	}
+}
