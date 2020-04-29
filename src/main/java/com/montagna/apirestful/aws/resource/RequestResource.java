d30190cc453dad9bf45881d839339d47bf19bae2
Henrique La Montagna <henriquelamontagna@gmail.com>
2020-04-29 15:27:55 -0300

    Create RequestResource.java

---
+++ src/main/java/com/montagna/apirestful/aws/resource/RequestResource.java
@@ -0,0 +1,62 @@
+package com.montagna.apirestful.aws.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.montagna.apirestful.aws.domain.Request;
+import com.montagna.apirestful.aws.domain.RequestStage;
+import com.montagna.apirestful.aws.service.RequestService;
+import com.montagna.apirestful.aws.service.RequestStageService;
+
+@RestController
+@RequestMapping(value = "requests")
+public class RequestResource {
+	
+	@Autowired
+	private RequestService requestService;
+	
+	@Autowired
+	private RequestStageService requestStageService;
+	
+	@PostMapping
+	public ResponseEntity<Request> save(@RequestBody Request request){
+		Request createdRequest = requestService.save(request);
+		return ResponseEntity.status(HttpStatus.CREATED).body(createdRequest);
+	}
+	
+	@PutMapping("/{requestId}")
+	public ResponseEntity<Request> update(@PathVariable("requestId") Long id ,@RequestBody Request request){
+		request.setId(id);
+		Request updatedRequest = requestService.update(request);
+		return ResponseEntity.ok(updatedRequest);
+	}
+
+	@GetMapping("/{requestId}")
+	public ResponseEntity<Request> getById(@PathVariable("requestId") Long id){
+		Request request = requestService.getById(id);
+		return ResponseEntity.ok(request);
+		}
+	
+	@GetMapping
+	public ResponseEntity<List<Request>> listAll(){
+		List<Request> requests = requestService.listAll();
+		return ResponseEntity.ok(requests);
+	}
+	
+	//listAllByRequestId
+	@GetMapping("/{requestId}/request-stages")
+	public ResponseEntity<List<RequestStage>> listAllByRequestId(@PathVariable("requestId") Long id){
+		List<RequestStage> stages = requestStageService.listAllByRequestId(id);
+		return ResponseEntity.ok(stages);
+	}
+}
