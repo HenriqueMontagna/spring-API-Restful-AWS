265f7b862259a2595200c9b02c2e918292c7ac5f
Henrique La Montagna <henriquelamontagna@gmail.com>
2020-05-11 15:54:07 -0300

    Update UserService.java

--- src/main/java/com/montagna/apirestful/aws/service/UserService.java
+++ src/main/java/com/montagna/apirestful/aws/service/UserService.java
@@ -4,10 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.montagna.apirestful.aws.domain.User;
 import com.montagna.apirestful.aws.exception.NotFoundException;
+import com.montagna.apirestful.aws.model.PageModel;
+import com.montagna.apirestful.aws.model.PageRequestModel;
 import com.montagna.apirestful.aws.repository.UserRepository;
 import com.montagna.apirestful.aws.service.util.HashUtil;
 
@@ -40,6 +45,14 @@ public class UserService {
 	public List<User> listAll(){
 		List<User> list = repo.findAll();
 		return list;
+	}
+	
+	public PageModel<User> listAllOnLazyMode(PageRequestModel pr){
+		Pageable pageable = PageRequest.of(pr.getPage(), pr.getSize());
+		Page<User> page = repo.findAll(pageable);
+		
+		PageModel<User> pm = new PageModel<User>((int)page.getTotalElements(), page.getSize(), page.getTotalPages(), page.getContent());
+		return pm;
 	}
 	
 	public User login(String email, String password) {
