39e0cd870dfccc0cf5a2ce31b4ffdf9e9f97d8c5
Henrique La Montagna <henriquelamontagna@gmail.com>
2020-05-14 14:22:44 -0300

    Input Validation

--- src/main/java/com/montagna/apirestful/aws/DTO/UserUpdateRoleDTO.java
+++ src/main/java/com/montagna/apirestful/aws/DTO/UserUpdateRoleDTO.java
@@ -1,5 +1,7 @@
 package com.montagna.apirestful.aws.DTO;
 
+import javax.validation.constraints.NotNull;
+
 import com.montagna.apirestful.aws.domain.enums.Role;
 
 import lombok.Getter;
@@ -8,6 +10,7 @@ import lombok.Setter;
 @Getter @Setter
 public class UserUpdateRoleDTO {
 	
+	@NotNull(message = "Role Required")
 	private Role role;
 
 }
