f9886c250c1c3ab86ab6d359175002a20d18ec3e
Henrique La Montagna <henriquelamontagna@gmail.com>
2020-05-15 14:54:02 -0300

    Input Validation

--- src/main/java/com/montagna/apirestful/aws/resource/RequestStageResource.java
+++ src/main/java/com/montagna/apirestful/aws/resource/RequestStageResource.java
@@ -1,5 +1,7 @@
 package com.montagna.apirestful.aws.resource;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.montagna.apirestful.aws.DTO.RequestStageSaveDTO;
 import com.montagna.apirestful.aws.domain.RequestStage;
 import com.montagna.apirestful.aws.service.RequestStageService;
 
@@ -22,9 +25,9 @@ public class RequestStageResource {
 	
 	//save
 	@PostMapping
-	public ResponseEntity<RequestStage> save(@RequestBody RequestStage stage){
-		RequestStage createdStage = requestStageService.save(stage);
-		return ResponseEntity.status(HttpStatus.CREATED).body(stage);
+	public ResponseEntity<RequestStage> save(@RequestBody @Valid RequestStageSaveDTO stageDTO){
+		RequestStage createdStage = requestStageService.save(stageDTO.transformToStage());
+		return ResponseEntity.status(HttpStatus.CREATED).body(createdStage);
 	}
 	
 	//getById
